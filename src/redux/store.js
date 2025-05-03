f2d1cfc5a7556d294136d5dca64e9d2409b5b905
AsifMohammedSifat <asifmohammedsifat38@gmail.com>
2025-05-03 12:36:46 +0600

    back to previous version

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
 import { rootreducer } from "./rootreducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootreducer);
+const store = createStore(rootreducer,composeWithDevTools());
 
 export default store;
